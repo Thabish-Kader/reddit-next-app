b58e119f34523b5ad58e44c474cf58d88946a6a0
Thabish <kadertabish2@gmail.com>
2022-11-28 12:00:47 +0400

    header
    Signed-off-by: Thabish <kadertabish2@gmail.com>

---
+++ components/Header.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+type Props = {};
+
+export const Header = (props: Props) => {
+	return <div>Header</div>;
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import { Header } from "../components/Header";
 
 export default function Home() {
 	return (
@@ -7,7 +8,7 @@ export default function Home() {
 			<Head>
 				<title>Reddit Demo</title>
 			</Head>
-			<h1 className="text-red-500">hello</h1>
+			<Header />
 		</div>
 	);
 }
